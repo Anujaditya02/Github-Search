8bd01b3157c468560a3c98ec468d6787b5835ccc
Anujaditya02 <anujaditya02@gmail.com>
2020-10-02 19:38:11 +0530

    Initial commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDDbs2bKnl7aaVWFsVx9PuWUAtEzOKf5V4",
+    authDomain: "mygitapp01.firebaseapp.com",
+    databaseURL: "https://mygitapp01.firebaseio.com",
+    projectId: "mygitapp01",
+    storageBucket: "mygitapp01.appspot.com",
+    messagingSenderId: "749859813654",
+    appId: "1:749859813654:web:8f94dfd36b3711b2bc3837",
+    measurementId: "G-SSF2Z9BZP6"
+  };
